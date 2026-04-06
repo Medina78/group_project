2753fe81e51cc54537a80bb47079cab5137dbd0d
Rinor <gamingwithgoldustk@gmail.com>
2026-04-06 20:08:21 +0200

    changes

---
+++ script.js
@@ -0,0 +1,16 @@
+
+    const hamburger = document.getElementById('hamburger');
+    const navbarMenu = document.getElementById('navbar-menu');
+    
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        navbarMenu.classList.toggle('active');
+    });
+
+    const navLinks = navbarMenu.querySelectorAll('a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            hamburger.classList.remove('active');
+            navbarMenu.classList.remove('active');
+        });
+    });
